5b93f4d438eb25c8bfc308a7d2091da6623c3939
Srikanth <srikanth@ideas2it.com>
2018-09-10 20:21:33 +0530

    Side menu integration for responsive screen

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,15 @@
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
+import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
+import Hidden from '@material-ui/core/Hidden';
 import IconButton from '@material-ui/core/IconButton';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
-import { createStyles, withStyles } from '@material-ui/core/styles';
-import { Theme } from '@material-ui/core/styles/createMuiTheme';
+import { createStyles, Theme, withStyles } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import AccountCircle from '@material-ui/icons/AccountCircle';
@@ -18,48 +23,74 @@ import { changeTheme } from '../actions/theme/theme';
 import { IState } from '../reducers';
 import { themes } from '../themes/index';
 import globalTranslations from '../translations/global.json';
-const styles = ({colors, transparentBackground}: Theme) => 
+
+const drawerWidth = 240;
+
+const styles = ({breakpoints, palette, mixins}: Theme) =>
   createStyles({
+    drawerPaper: {
+      width: drawerWidth,
+      [breakpoints.up('md')]: {
+        position: 'relative',
+      },
+    },
     dropdownBtn: {
-      color: colors.primaryText,
-      textTransform: 'capitalize'
+      '&:hover': {
+        backgroundColor: palette.secondary.light
+      },
+      color: palette.primary.light,
+      textTransform: 'capitalize',
     },
     flex: {
+      alignItems: 'center',
+      display: 'flex',
       flexGrow: 1,
+      justifyContent: 'flex-end',
     },
     linkBtn: {
       '&.active': {
-        backgroundColor: transparentBackground.dark
+        backgroundColor: palette.secondary.light
       },
       '&:hover': {
-        backgroundColor: transparentBackground.dark
+        backgroundColor: palette.secondary.light
       },
       borderRadius: '3px',
-      color: colors.primaryText,
+      color: palette.primary.light,
       margin: '0 3px',
       padding: '5px 10px',
       textDecoration: 'none',
+    },
+    navIconHide: {
+      [breakpoints.up('md')]: {
+        display: 'none',
+      },
+      flex: 0,
+      'margin-left': 'auto',
     },
     root: {
       display: 'flex',
       flexDirection: 'column',
       marginBottom: '10px'
     },
-});
-
+    toolbar: mixins.toolbar,
+  });
 
 interface INavState {
   languageSelection: any,
   themeSelection: any,
-  availableThemes: string[]
+  availableThemes: string[],
+  openSideNav: boolean,
+  direction: string
 }
 
 class NavBar extends React.Component<INavProps, INavState>{
   constructor(props: INavProps) {
     super(props);
     this.state = {
-      availableThemes: (()=> Object.keys(themes))(),
+      availableThemes: (() => Object.keys(themes))(),
+      direction: 'rtl',
       languageSelection: null,
+      openSideNav: false,
       themeSelection: null,
     }
     this.props.initialize({
@@ -73,20 +104,17 @@ class NavBar extends React.Component<INavProps, INavState>{
     });
   }
 
-  public componentDidUpdate(prevProps: INavProps) {
-    const prevLangCode = prevProps.activeLanguage && prevProps.activeLanguage.code;
-    const curLangCode = this.props.activeLanguage && this.props.activeLanguage.code;
-    const hasLanguageChanged = prevLangCode !== curLangCode;
-    // tslint:disable-next-line:no-console
-    console.log('test', hasLanguageChanged, this.props);
-  }
-  
-  public handleClick = (event: any) => {
+  public handleDrawerToggle = () => {
+    this.setState(state => ({ openSideNav: !state.openSideNav }));
+  };
+
+  public handleLangClick = (event: any) => {
     this.setState({ languageSelection: event.currentTarget });
   };
 
-  public handleClose = (code: string) => {
+  public handleLangClose = (code: string) => {
     this.props.setActiveLanguage(code);
+    this.handleDrawerToggle();
     this.setState({ languageSelection: null });
   };
 
@@ -95,6 +123,7 @@ class NavBar extends React.Component<INavProps, INavState>{
   };
 
   public handleThemeClose = (theme: string) => {
+    this.handleDrawerToggle();
     this.props.setActiveTheme(theme);
     this.setState({ themeSelection: null });
   };
@@ -102,16 +131,77 @@ class NavBar extends React.Component<INavProps, INavState>{
   public render() {
     const { activeLanguage, activeTheme, classes, languages } = this.props;
     const { availableThemes, languageSelection, themeSelection } = this.state;
-    return (
-      <div className={classes.root}>
-        <AppBar position="static">
-          <Toolbar variant="dense">
-            <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="title" color="inherit" className={classes.flex}>
-              App Name
-            </Typography>
+    const navLinks = (
+      <div className={classes.flex}>
+        <NavLink
+          exact={true}
+          to="/"
+          activeClassName="active"
+          className={classes.linkBtn}>
+          Home
+        </NavLink>
+        <NavLink
+          exact={true}
+          to="/hello"
+          activeClassName="active"
+          className={classes.linkBtn}>
+          Hello
+        </NavLink>
+        <NavLink
+          exact={true}
+          to="/counter"
+          activeClassName="active"
+          className={classes.linkBtn}>
+          Counter
+        </NavLink>
+        <Button
+          className={classes.dropdownBtn}
+          aria-owns={languageSelection ? 'simple-menu' : undefined}
+          aria-haspopup="true"
+          onClick={this.handleLangClick}>
+          {activeLanguage ? activeLanguage.name : ''}
+        </Button>
+        <Menu
+          id="simple-menu"
+          anchorEl={languageSelection}
+          open={Boolean(languageSelection)}
+          onClose={this.handleLangClose.bind(this, activeLanguage ? activeLanguage.code : '')}>
+          {languages.map((lang, index) => (
+            <MenuItem key={index} onClick={this.handleLangClose.bind(this, lang.code)}>{lang.name}</MenuItem>
+          ))}
+        </Menu>
+        <Button
+          className={classes.dropdownBtn}
+          aria-owns={themeSelection ? 'simple-menu' : undefined}
+          aria-haspopup="true"
+          onClick={this.handleThemeClick}>
+          {activeTheme}
+        </Button>
+        <Menu
+          id="simple-menu"
+          anchorEl={themeSelection}
+          open={Boolean(themeSelection)}
+          onClose={this.handleThemeClose.bind(this, activeTheme)}>
+          {availableThemes.map((theme, index) => (
+            <MenuItem key={index} onClick={this.handleThemeClose.bind(this, theme)}>{theme}</MenuItem>
+          ))}
+        </Menu>
+        <IconButton
+          aria-haspopup="true"
+          color="inherit">
+          <AccountCircle />
+        </IconButton>
+      </div>
+    );
+
+    const sideNavLinks = (
+      <div className={classes.toolbar}>
+        <List onClick={this.handleDrawerToggle}>
+          <ListItem button={true}>
+            <ListItemText primary="Main Menu" />
+          </ListItem>
+          <Divider />
+          <ListItem>
             <NavLink
               exact={true}
               to="/"
@@ -119,6 +209,8 @@ class NavBar extends React.Component<INavProps, INavState>{
               className={classes.linkBtn}>
               Home
             </NavLink>
+          </ListItem>
+          <ListItem>
             <NavLink
               exact={true}
               to="/hello"
@@ -126,6 +218,8 @@ class NavBar extends React.Component<INavProps, INavState>{
               className={classes.linkBtn}>
               Hello
             </NavLink>
+          </ListItem>
+          <ListItem>
             <NavLink
               exact={true}
               to="/counter"
@@ -133,57 +227,92 @@ class NavBar extends React.Component<INavProps, INavState>{
               className={classes.linkBtn}>
               Counter
             </NavLink>
-            <div>
-              <Button
-                className={classes.dropdownBtn}
-                aria-owns={languageSelection ? 'simple-menu' : undefined}
-                aria-haspopup="true"
-                onClick={this.handleClick}
-              >
-                {activeLanguage? activeLanguage.name : ''}
-              </Button>
-              <Menu
-                id="simple-menu"
-                anchorEl={languageSelection}
-                open={Boolean(languageSelection)}
-                onClose={this.handleClose.bind}
-              >
-              {languages.map((lang, index) => (
-                <MenuItem key={index} onClick={this.handleClose.bind(this, lang.code)}>{lang.name}</MenuItem>
-              ))}
-              </Menu>
-              <Button
-                className={classes.dropdownBtn}
-                aria-owns={themeSelection ? 'simple-menu' : undefined}
-                aria-haspopup="true"
-                onClick={this.handleThemeClick}
-              >
-                {activeTheme}
-              </Button>
-              <Menu
-                id="simple-menu"
-                anchorEl={themeSelection}
-                open={Boolean(themeSelection)}
-                onClose={this.handleThemeClose.bind}
-              >
-              {availableThemes.map((theme, index) => (
-                <MenuItem key={index} onClick={this.handleThemeClose.bind(this, theme)}>{theme}</MenuItem>
-              ))}
-              </Menu>
-            </div>
+          </ListItem>
+        </List>
+        <ListItem >
+          <Button
+            className={classes.dropdownBtn}
+            aria-owns={languageSelection ? 'simple-menu' : undefined}
+            aria-haspopup="true"
+            onClick={this.handleLangClick}>
+            {activeLanguage ? activeLanguage.name : ''}
+          </Button>
+          <Menu
+            id="simple-menu"
+            anchorEl={languageSelection}
+            open={Boolean(languageSelection)}
+            onClose={this.handleLangClose.bind(this, activeLanguage ? activeLanguage.code : '')}>
+            {languages.map((lang, index) => (
+              <MenuItem key={index} onClick={this.handleLangClose.bind(this, lang.code)}>{lang.name}</MenuItem>
+            ))}
+          </Menu>
+        </ListItem>
+        <ListItem>
+          <Button
+            className={classes.dropdownBtn}
+            aria-owns={themeSelection ? 'simple-menu' : undefined}
+            aria-haspopup="true"
+            onClick={this.handleThemeClick}>
+            {activeTheme}
+          </Button>
+          <Menu
+            id="simple-menu"
+            anchorEl={themeSelection}
+            open={Boolean(themeSelection)}
+            onClose={this.handleThemeClose.bind(this, activeTheme)}>
+            {availableThemes.map((theme, index) => (
+              <MenuItem key={index} onClick={this.handleThemeClose.bind(this, theme)}>{theme}</MenuItem>
+            ))}
+          </Menu>
+        </ListItem>
+        <ListItem>
+          <IconButton
+            aria-haspopup="true"
+            color="inherit">
+            <AccountCircle />
+          </IconButton>
+        </ListItem>
+        <Divider />
+      </div>
+    );
+
+    return (
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar variant="dense">
+            <Typography variant="title" color="inherit">
+              App Name
+            </Typography>
+            <Hidden smDown={true} className={classes.flex}>
+              {navLinks}
+            </Hidden>
             <IconButton
-              aria-haspopup="true"
               color="inherit"
-            >
-              <AccountCircle />
+              aria-label="Open drawer"
+              onClick={this.handleDrawerToggle}
+              className={classes.navIconHide}>
+              <MenuIcon />
             </IconButton>
           </Toolbar>
         </AppBar>
+        <Hidden mdUp={true}>
+          <Drawer
+            anchor={this.state.direction === 'rtl' ? 'right' : 'left'}
+            open={this.state.openSideNav}
+            onClose={this.handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}>
+            {sideNavLinks}
+          </Drawer>
+        </Hidden>
       </div>
     )
   }
 }
-
 
 interface INavProps extends LocalizeContextProps {
   classes: any,
@@ -207,5 +336,4 @@ const mapDispatchToProps = (dispatch: any) => ({
   setActiveTheme: (theme: string) => dispatch(changeTheme(theme))
 })
 
-
-export default connect<IStateProps, IDispatchProps>(mapStateToProps, mapDispatchToProps)(withStyles(styles)(withLocalize(NavBar)));
+export default connect<IStateProps, IDispatchProps>(mapStateToProps, mapDispatchToProps)(withStyles(styles, { withTheme: true })(withLocalize(NavBar)));
