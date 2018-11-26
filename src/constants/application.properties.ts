d6f0239f6e9c3d87d90f1011b33e364675c3da57
MukundhanSampath <mukundhan@ideas2it.com>
2018-11-26 21:55:32 +0530

    Changes: Added environmental variables for baseUrl  - declared toastify package globally

--- src/constants/application.properties.ts
+++ src/constants/application.properties.ts
@@ -1,8 +1,7 @@
-// DEV Server URL: http://35.237.114.118:3030
 export const AppProperties = {
   ACCESS_TOKEN_KEY: 'accessToken',
   ACTIVE_THEME: 'activeTheme',
-  BASE_URL: 'http://192.168.15.108:3030',
+  BASE_URL: process.env.REACT_APP_BASE_URL,
   DEFAULT_LANGUAGE: {
     code: 'en',
     name: 'English'
--- typings/misc.d.ts
+++ typings/misc.d.ts
@@ -1,6 +1,7 @@
 declare module "jss-preset-default";
 declare module "react-jss/*";
 declare module "key-mirror";
+declare module "react-toastify";
 
 // type declerations for Sending api requests with custom filters.
 declare module "request-filter" {
